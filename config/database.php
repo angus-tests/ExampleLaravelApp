14d71fc2aaa61ff4012e9d81a6c8107e34104259
Angus <angus.goody@gmail.com>
2023-11-05 14:29:55 +0000

    updated build

--- config/database.php
+++ config/database.php
@@ -50,13 +50,16 @@ return [
         ],
 
         'mysql' => [
+                    
+
+        'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST') ?: $_SERVER['RDS_HOSTNAME'],
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE') ?: $_SERVER['RDS_DB_NAME'],
+            'username' => env('DB_USERNAME') ?: $_SERVER['RDS_USERNAME'],
+            'password' => env('DB_PASSWORD') ?: $_SERVER['RDS_PASSWORD'],
+            'url' => env('DATABASE_URL'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
